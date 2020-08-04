4263133848c378679865bd186365b7a897717d2e
Chuntong Gao <heihegao@gmail.com>
2020-08-04 14:19:33 -0700

    change production reader url

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,5 @@
 /* eslint-disable import/no-mutable-exports */
-let BLOGGY_READER_BASE_URL = 'https://bloggy2020.herokuapp.com/reader/publisher';
+let BLOGGY_READER_BASE_URL = 'https://bloggy2020.herokuapp.com/reader/#/publisher';
 if (process.env.NODE_ENV === 'development') {
     BLOGGY_READER_BASE_URL = 'http://localhost:2000/publisher';
 }
